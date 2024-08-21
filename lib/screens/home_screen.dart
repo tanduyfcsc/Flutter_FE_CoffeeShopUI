aee577426db783f090c80438697696e59e3a9a8a
tanduyfcsc <tanduyfcsc@gmail.com>
2024-08-21 16:44:51 +0700

    Add assets images, create WelcomeScreen and HomeScreen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget{
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
